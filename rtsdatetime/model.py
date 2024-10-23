f64a91729df40bf6f3e0e6cddb6c024d052c95e3
ripple <github@ripple.contact>
2024-10-23 19:01:10 +0100

    init commit

---
+++ rtsdatetime/model.py
@@ -0,0 +1,337 @@
+import datetime
+import enum
+from math import ceil
+import re
+
+
+class TimeUnits(enum.Enum):
+    BEAT = 1
+    DECITAP = 2
+    TAP = 3
+    HEXA = 4
+    OCTA = 5
+    ROLLING_OCTA = 6
+    DAY = 7
+    YEAR = 8
+    QUADRENNIAL = 9
+    SECONDS = 10
+    MINUTE = 11
+    HOUR = 12
+    ROLLING_HOUR = 13
+
+
+UNIT_LENGTHS = {
+    TimeUnits.BEAT: 40,
+    TimeUnits.DECITAP: 10,
+    TimeUnits.TAP: 6,
+    TimeUnits.HEXA: 8,
+    TimeUnits.OCTA: 6,
+    TimeUnits.ROLLING_OCTA: 0,
+    TimeUnits.DAY: 0,
+    TimeUnits.YEAR: 0,
+    TimeUnits.QUADRENNIAL: 0,
+    TimeUnits.SECONDS: 60,
+    TimeUnits.MINUTE: 60,
+    TimeUnits.HOUR: 24,
+    TimeUnits.ROLLING_HOUR: 0,
+}
+
+UNIT_BEATS = {
+    TimeUnits.BEAT: 1,
+    TimeUnits.DECITAP: 40,
+    TimeUnits.TAP: 400,
+    TimeUnits.HEXA: 2400,
+    TimeUnits.OCTA: 19200,
+    TimeUnits.ROLLING_OCTA: 19200,
+    TimeUnits.DAY: 115200,
+    TimeUnits.YEAR: 42077520,
+    TimeUnits.QUADRENNIAL: 168310080,
+    TimeUnits.SECONDS: 4 / 3,
+    TimeUnits.MINUTE: 80,
+    TimeUnits.HOUR: 4800,
+    TimeUnits.ROLLING_HOUR: 4800,
+}
+
+
+class RTSTimeDelta(object):
+    def __init__(self, timediff: int):
+        self.timediff = timediff
+        self.quadrennial = self._timestamp_to_unit(TimeUnits.QUADRENNIAL)
+        self.year = self._timestamp_to_unit(TimeUnits.YEAR)
+        self.day = self._timestamp_to_unit(TimeUnits.DAY)
+
+        # rst time
+        self.octa = self._timestamp_to_unit(TimeUnits.OCTA)
+        self.hexa = self._timestamp_to_unit(TimeUnits.HEXA)
+        self.tap = self._timestamp_to_unit(TimeUnits.TAP)
+        self.decitap = self._timestamp_to_unit(TimeUnits.DECITAP)
+        self.beat = self._timestamp_to_unit(TimeUnits.BEAT)
+
+        # utc time
+        self.seconds = self._timestamp_to_unit(TimeUnits.SECONDS)
+        self.minute = self._timestamp_to_unit(TimeUnits.MINUTE)
+        self.hour = self._timestamp_to_unit(TimeUnits.HOUR)
+
+    @classmethod
+    def from_units(
+        cls,
+        quadrienials: float = 0,
+        years: float = 0,
+        days: float = 0,
+        octas: float = 0,
+        hexas: float = 0,
+        taps: float = 0,
+        decitaps: float = 0,
+        beats: float = 0,
+        hours: float = 0,
+        minutes: float = 0,
+        seconds: float = 0,
+    ):
+        timediff = 0
+        # rst date
+        timediff += quadrienials * UNIT_BEATS[TimeUnits.QUADRENNIAL]
+        timediff += years * UNIT_BEATS[TimeUnits.YEAR]
+        timediff += days * UNIT_BEATS[TimeUnits.DAY]
+
+        # rst time
+        timediff += octas * UNIT_BEATS[TimeUnits.OCTA]
+        timediff += hexas * UNIT_BEATS[TimeUnits.HEXA]
+        timediff += taps * UNIT_BEATS[TimeUnits.TAP]
+        timediff += decitaps * UNIT_BEATS[TimeUnits.DECITAP]
+        timediff += beats * UNIT_BEATS[TimeUnits.BEAT]
+
+        # utc time
+        timediff += seconds * UNIT_BEATS[TimeUnits.SECONDS]
+        timediff += minutes * UNIT_BEATS[TimeUnits.MINUTE]
+        timediff += hours * UNIT_BEATS[TimeUnits.HOUR]
+        timediff = int(timediff)
+        return cls(timediff)
+
+    def _timestamp_to_unit(self, unit: TimeUnits):
+        if UNIT_LENGTHS[unit]:
+            positive_unit = int(abs(self.timediff) // UNIT_BEATS[unit] % UNIT_LENGTHS[unit])
+        else:
+            positive_unit = abs(self.timediff) // UNIT_BEATS[unit]
+        if self.timediff < 0:
+            return -positive_unit
+        return positive_unit
+
+    def __add__(self, other):
+        if type(other) is not RTSTimeDelta:
+            raise TypeError(f"unsupported operand type(s) for +: 'RSTimeDelta' and '{type(other)}'")
+        return RTSTimeDelta(self.timediff + other.timediff)
+
+    def __sub__(self, other):
+        if type(other) is not RTSTimeDelta:
+            raise TypeError(f"unsupported operand type(s) for +: 'RSTimeDelta' and '{type(other)}'")
+        return RTSTimeDelta(self.timediff - other.timediff)
+
+
+class RTSTimeComponent(object):
+    UNIT_NEGATIVE_OFFSET = {
+        TimeUnits.BEAT: 1,
+        TimeUnits.DECITAP: 1,
+        TimeUnits.TAP: 1,
+        TimeUnits.HEXA: 1,
+        TimeUnits.OCTA: 1,
+        TimeUnits.ROLLING_OCTA: 0,
+        TimeUnits.DAY: 1,
+        TimeUnits.YEAR: 0,
+        TimeUnits.QUADRENNIAL: 0,
+        TimeUnits.SECONDS: 1,
+        TimeUnits.MINUTE: 1,
+        TimeUnits.HOUR: 1,
+        TimeUnits.ROLLING_HOUR: 0,
+    }
+
+    BEAT_LENGTH_SECOND = 0.75
+
+    def __init__(self, timestamp: int, name: str):
+        self.name = name
+        self.timestamp = timestamp
+        # rst date
+        self.quadrennial = self._timestamp_to_unit(TimeUnits.QUADRENNIAL)
+        self.year = self._timestamp_to_unit(TimeUnits.YEAR)
+        self.day = self._timestamp_to_unit(TimeUnits.DAY)
+
+        # rst time
+        self.rolling_octa = self._timestamp_to_unit(TimeUnits.ROLLING_OCTA)
+        self.octa = self._timestamp_to_unit(TimeUnits.OCTA)
+        self.hexa = self._timestamp_to_unit(TimeUnits.HEXA)
+        self.tap = self._timestamp_to_unit(TimeUnits.TAP)
+        self.decitap = self._timestamp_to_unit(TimeUnits.DECITAP)
+        self.beat = self._timestamp_to_unit(TimeUnits.BEAT)
+
+        # utc time
+        self.seconds = self._timestamp_to_unit(TimeUnits.SECONDS)
+        self.minute = self._timestamp_to_unit(TimeUnits.MINUTE)
+        self.hour = self._timestamp_to_unit(TimeUnits.HOUR)
+        self.rolling_hour = self._timestamp_to_unit(TimeUnits.ROLLING_HOUR)
+
+    @classmethod
+    def now(cls, name: str, epoch: int):
+        time = (datetime.datetime.now().timestamp() - epoch) / cls.BEAT_LENGTH_SECOND
+        return cls(int(time), name)
+
+    @classmethod
+    def from_timestamp(cls, timestamp: str):
+        if not re.match("^\w+:\-?\d+$", timestamp):
+            raise ValueError(f"Invalid timestamp: {timestamp}")
+        name, timestamp = timestamp.split(":")
+        timestamp_value = int(timestamp)
+        return cls(timestamp_value, name)
+
+    @classmethod
+    def from_units(
+        cls,
+        name: str,
+        quadrienials: int = 0,
+        years: int = 0,
+        days: int = 0,
+        octas: int = 0,
+        hexas: int = 0,
+        taps: int = 0,
+        decitaps: int = 0,
+        beats: int = 0,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
+    ):
+        timestamp = 0
+        # rst date
+        timestamp += quadrienials * UNIT_BEATS[TimeUnits.QUADRENNIAL]
+        timestamp += years * UNIT_BEATS[TimeUnits.YEAR]
+        timestamp += days * UNIT_BEATS[TimeUnits.DAY]
+
+        # rst time
+        timestamp += octas * UNIT_BEATS[TimeUnits.OCTA]
+        timestamp += hexas * UNIT_BEATS[TimeUnits.HEXA]
+        timestamp += taps * UNIT_BEATS[TimeUnits.TAP]
+        timestamp += decitaps * UNIT_BEATS[TimeUnits.DECITAP]
+        timestamp += beats * UNIT_BEATS[TimeUnits.BEAT]
+
+        # utc time
+        timestamp += seconds * UNIT_BEATS[TimeUnits.SECONDS]
+        timestamp += minutes * UNIT_BEATS[TimeUnits.MINUTE]
+        timestamp += hours * UNIT_BEATS[TimeUnits.HOUR]
+        return cls(ceil(timestamp), name)
+
+    @classmethod
+    def from_time_component(cls, time_component: "RTSTimeComponent", name: str | None = None):
+        return cls(time_component.timestamp, name if name is not None else time_component.name)
+
+    def _timestamp_to_unit(self, unit: TimeUnits):
+        timestamp = self.timestamp + 1 if self.timestamp < 0 else self.timestamp
+        if UNIT_LENGTHS[unit]:
+            positive_unit = int(abs(timestamp) // UNIT_BEATS[unit] % UNIT_LENGTHS[unit])
+        else:
+            positive_unit = abs(timestamp) // UNIT_BEATS[unit]
+        if self.timestamp < 0:
+            return UNIT_LENGTHS[unit] - positive_unit - self.UNIT_NEGATIVE_OFFSET[unit]
+        return positive_unit
+
+    def add(self, timedelta: RTSTimeDelta, new_name: str | None = None):
+        if type(timedelta) is not RTSTimeDelta:
+            raise TypeError(f"unsupported operand type(s) for +: 'RSTimeComponent' and '{type(timedelta)}'")
+        return RTSTimeComponent(
+            self.timestamp + timedelta.timediff,
+            self.name if new_name is None else new_name,
+        )
+
+    def sub(self, timedelta: RTSTimeDelta, new_name: str | None = None):
+        if type(timedelta) is not RTSTimeDelta:
+            raise TypeError(f"unsupported operand type(s) for +: 'RSTimeComponent' and '{type(timedelta)}'")
+        return RTSTimeComponent(self.timestamp - timedelta.timediff, self.name if new_name is None else new_name)
+
+    def format(self, format_str):
+        fmt_values = {
+            f"[{self.name}.QU]": self.quadrennial,
+            f"[{self.name}.YE]": self.year,
+            f"[{self.name}.DA]": self.day,
+            f"[{self.name}.OC]": self.octa,
+            f"[{self.name}.HE]": self.hexa,
+            f"[{self.name}.TA]": self.tap,
+            f"[{self.name}.DE]": self.decitap,
+            f"[{self.name}.BE]": self.beat,
+            f"[{self.name}.SE]": self.seconds,
+            f"[{self.name}.MI]": self.minute,
+            f"[{self.name}.HO]": self.hour,
+            f"[{self.name}.RH]": self.rolling_hour,
+            f"[{self.name}.RO]": self.rolling_octa,
+        }
+        for key, value in fmt_values.items():
+            format_str = format_str.replace(key, str(value))
+        return format_str
+
+    def to_timestamp(self):
+        return f"{self.name}:{self.timestamp}"
+
+    def __add__(self, other):
+        return self.add(other)
+
+    def __sub__(self, other):
+        return self.sub(other)
+
+    def __eq__(self, value: object) -> bool:
+        if type(value) is not RTSTimeComponent:
+            return False
+        return self.timestamp == value.timestamp
+
+    def __repr__(self):
+        quadrennials = f"quadrennials={self.quadrennial}, " if self.quadrennial else ""
+        days = f"days={self.day}, " if self.day else ""
+        years = f"years={self.year}, " if self.year else ""
+        octas = f"octas={self.octa}, " if self.octa else ""
+        hexas = f"hexas={self.hexa}, " if self.hexa else ""
+        taps = f"taps={self.tap}, " if self.tap else ""
+        decitaps = f"decitaps={self.decitap}, " if self.decitap else ""
+        beats = f"beats={self.beat}, " if self.beat else ""
+        comp_str = f"{quadrennials}{days}{years}{octas}{hexas}{taps}{decitaps}{beats}"[:-2]
+        return f"RSTimeComponent.from_units({comp_str})"
+
+
+class RTSDateTime(object):
+    def __init__(self, time_components: list[RTSTimeComponent], static_time_components: list[RTSTimeComponent]):
+        self._ticking_components = time_components
+        self._static_components = static_time_components
+        self._components = {component.name: component for component in self._ticking_components + self._static_components}
+
+    @classmethod
+    def from_timestamp(cls, timestamp: str):
+        ticking_components, static_components = timestamp.split(".")
+        return cls(
+            [RTSTimeComponent.from_timestamp(component) for component in ticking_components.split(",") if component],
+            [RTSTimeComponent.from_timestamp(component) for component in static_components.split(",") if component],
+        )
+
+    def __add__(self, other):
+        if type(other) is not RTSTimeDelta:
+            raise TypeError(f"unsupported operand type(s) for +: 'RSTimeDate' and '{type(other)}'")
+        return RTSDateTime(
+            [component + other for component in self._ticking_components],
+            self._static_components,
+        )
+
+    def __sub__(self, other):
+        if type(other) is not RTSTimeDelta:
+            raise TypeError(f"unsupported operand type(s) for -: 'RSTimeDate' and '{type(other)}'")
+        return RTSDateTime(
+            [component - other for component in self._ticking_components],
+            self._static_components,
+        )
+
+    def format(self, format_str: str):
+        for component in self._ticking_components + self._static_components:
+            format_str = component.format(format_str)
+        return format_str
+
+    def to_timestamp(self):
+        component_str = ",".join([component.to_timestamp() for component in self._ticking_components])
+        static_component_str = ",".join([component.to_timestamp() for component in self._static_components])
+        return f"{component_str}.{static_component_str}"
+
+    def __getitem__(self, key: str):
+        return self._components[key]
+
+    def __repr__(self):
+        return f"RSTimeDate({self._ticking_components}, {self._static_components})"
---
+++ tests/model/test_rstcomponent.py
@@ -0,0 +1,376 @@
+from curses.ascii import RS
+from re import T
+import pytest
+from rtsdatetime.model import RTSTimeComponent, RTSTimeDelta
+
+
+def component_tester(rst_component: RTSTimeComponent, test_results: dict[str, int]):
+    assert rst_component.timestamp == test_results.get("timestamp", 0)
+    # rst date
+    assert rst_component.year == test_results.get("year", 0)
+    assert rst_component.quadrennial == test_results.get("quadrennial", 0)
+    assert rst_component.day == test_results.get("day", 0)
+
+    # rst time
+    assert rst_component.rolling_octa == test_results.get("rolling_octa", 0)
+    assert rst_component.octa == test_results.get("octa", 0)
+    assert rst_component.hexa == test_results.get("hexa", 0)
+    assert rst_component.tap == test_results.get("tap", 0)
+    assert rst_component.decitap == test_results.get("decitap", 0)
+    assert rst_component.beat == test_results.get("beat", 0)
+
+    # utc time
+    assert rst_component.rolling_hour == test_results.get("rolling_hour", 0)
+    assert rst_component.hour == test_results.get("hour", 0)
+    assert rst_component.minute == test_results.get("minute", 0)
+    assert rst_component.seconds == test_results.get("seconds", 0)
+    assert rst_component.name == test_results.get("name", "")
+
+
+def test_timestamp():
+    timestamps = [
+        ("test", 12345678),
+        ("test2", 20),
+        ("test3", 1234567),
+        ("test4", 3475753),
+        ("test5", 3475753),
+        ("test6", -1234567),
+        ("test7", -3475753),
+        ("test8", -3475753),
+    ]
+    for name, timestamp in timestamps:
+        rst_component = RTSTimeComponent.from_timestamp(f"{name}:{timestamp}")
+        assert rst_component.timestamp == timestamp
+        assert rst_component.name == name
+
+    timestamps = ["test:1234a5678", "12312323", "test:123451a", "test:123451 a", "test:123451 test:123451", "test:--123421313"]
+    for timestamp in timestamps:
+        with pytest.raises(ValueError):
+            rst_component = RTSTimeComponent.from_timestamp(timestamp)
+
+
+def test_from_units():
+    test_data = [
+        (
+            {"days": 367, "octas": 2, "hexas": 3, "taps": 4, "decitaps": 5, "beats": 6, "name": "test"},
+            {
+                "timestamp": 42325806,
+                "year": 1,
+                "day": 367,
+                "rolling_octa": 2204,
+                "octa": 2,
+                "hexa": 3,
+                "tap": 4,
+                "decitap": 5,
+                "beat": 6,
+                "rolling_hour": 8817,
+                "hour": 9,
+                "minute": 52,
+                "seconds": 34,
+                "name": "test",
+            },
+        ),
+        (
+            {"name": "test"},
+            {"name": "test"},
+        ),
+        (
+            {"days": -1, "beats": 2, "name": "test"},
+            {
+                "timestamp": -115198,
+                "day": -1,
+                "rolling_octa": -5,
+                "beat": 2,
+                "rolling_hour": -23,
+                "seconds": 2,
+                "name": "test",
+            },
+        ),
+        (
+            {"days": -1, "beats": -2, "name": "test"},
+            {
+                "timestamp": -115202,
+                "day": -2,
+                "rolling_octa": -6,
+                "octa": 5,
+                "hexa": 7,
+                "tap": 5,
+                "decitap": 9,
+                "beat": 38,
+                "rolling_hour": -24,
+                "hour": 23,
+                "minute": 59,
+                "seconds": 59,
+                "name": "test",
+            },
+        ),
+        (
+            {"days": 1, "hours": 20, "minutes": 50, "seconds": 10, "name": "test"},
+            {
+                "timestamp": 215214,
+                "day": 1,
+                "rolling_octa": 11,
+                "octa": 5,
+                "hexa": 1,
+                "tap": 4,
+                "beat": 14,
+                "rolling_hour": 44,
+                "hour": 20,
+                "minute": 50,
+                "seconds": 10,
+                "name": "test",
+            },
+        ),
+        (
+            {"years": 1, "name": "test"},
+            {
+                "timestamp": 42077520,
+                "year": 1,
+                "day": 365,
+                "rolling_octa": 2191,
+                "octa": 1,
+                "hexa": 4,
+                "tap": 1,
+                "decitap": 8,
+                "rolling_hour": 8766,
+                "hour": 6,
+                "minute": 9,
+                "name": "test",
+            },
+        ),
+    ]
+    # test all units existing
+    for test_args, test_results in test_data:
+        rst_component = RTSTimeComponent.from_units(**test_args)
+        component_tester(rst_component, test_results)
+
+
+def test_addition():
+    additions = [
+        (
+            {"beats": 15, "octas": 2, "name": "test"},
+            {"beats": 15, "octas": 2},
+            {
+                "timestamp": 76830,
+                "beat": 30,
+                "octa": 4,
+                "rolling_octa": 4,
+                "rolling_hour": 16,
+                "hour": 16,
+                "minute": 0,
+                "seconds": 22,
+                "name": "test",
+            },
+        ),
+        (
+            {"beats": 15, "octas": 2, "days": 1, "name": "test"},
+            {"beats": 15, "octas": 2},
+            {
+                "timestamp": 192030,
+                "beat": 30,
+                "octa": 4,
+                "day": 1,
+                "rolling_octa": 10,
+                "rolling_hour": 40,
+                "hour": 16,
+                "minute": 0,
+                "seconds": 22,
+                "name": "test",
+            },
+        ),
+        (
+            {"beats": 15, "octas": 2, "days": 1, "name": "test"},
+            {"beats": 15, "octas": 4},
+            {
+                "timestamp": 230430,
+                "beat": 30,
+                "day": 2,
+                "rolling_octa": 12,
+                "rolling_hour": 48,
+                "hour": 0,
+                "minute": 0,
+                "seconds": 22,
+                "name": "test",
+            },
+        ),
+    ]
+
+    for test_comargs, test_diffargs, test_results in additions:
+        date = RTSTimeComponent.from_units(**test_comargs)
+        adder = RTSTimeDelta.from_units(**test_diffargs)
+
+        new_date = date + adder
+        component_tester(new_date, test_results)
+
+        new_date = date.add(adder, "new_name")
+        test_results["name"] = "new_name"
+        component_tester(new_date, test_results)
+
+        with pytest.raises(TypeError):
+            date + 1
+        with pytest.raises(TypeError):
+            date + 1.0
+        with pytest.raises(TypeError):
+            date + "abc"
+
+
+def test_subtraction():
+    subtractions = [
+        (
+            {"beats": 15, "octas": 2, "name": "test"},
+            {"beats": 15, "octas": 2},
+            {"name": "test"},
+        ),
+        (
+            {"beats": 15, "octas": 2, "days": 1, "name": "test"},
+            {"beats": 15, "octas": 2},
+            {"timestamp": 115200, "day": 1, "rolling_octa": 6, "rolling_hour": 24, "name": "test"},
+        ),
+        (
+            {"beats": 15, "octas": 2, "days": 1, "name": "test"},
+            {"beats": 15, "octas": 4},
+            {"timestamp": 76800, "rolling_octa": 4, "octa": 4, "rolling_hour": 16, "hour": 16, "name": "test"},
+        ),
+        (
+            {"beats": 15, "octas": 2, "days": 1, "name": "test"},
+            {"beats": 15, "octas": 4, "days": 1},
+            {"timestamp": -38400, "rolling_octa": -1, "day": -1, "octa": 4, "rolling_hour": -7, "hour": 16, "name": "test"},
+        ),
+    ]
+
+    for test_comargs, test_diffargs, test_results in subtractions:
+        date = RTSTimeComponent.from_units(**test_comargs)
+        adder = RTSTimeDelta.from_units(**test_diffargs)
+        new_date = date - adder
+        component_tester(new_date, test_results)
+
+        new_date = date.sub(adder, "new_name")
+        test_results["name"] = "new_name"
+        component_tester(new_date, test_results)
+
+        with pytest.raises(TypeError):
+            date - 1
+        with pytest.raises(TypeError):
+            date - 1.0
+        with pytest.raises(TypeError):
+            date - "abc"
+
+
+def test_equality():
+    equal = [
+        (
+            {"beats": 15, "octas": 2, "name": "test"},
+            {"beats": 15, "octas": 2, "name": "test"},
+        ),
+        (
+            {"beats": 15, "octas": 2, "days": 1, "name": "test"},
+            {"beats": 15, "octas": 2, "days": 1, "name": "test"},
+        ),
+    ]
+
+    for test_a, test_b in equal:
+        tast_tc1 = RTSTimeComponent.from_units(**test_a)
+        test_tc2 = RTSTimeComponent.from_units(**test_b)
+        assert tast_tc1 == test_tc2
+
+    not_equal = [
+        (
+            {"beats": 15, "octas": 2, "name": "test"},
+            {"beats": 14, "octas": 2, "name": "test"},
+        ),
+        (
+            {"beats": 15, "octas": 2, "days": 1, "name": "test"},
+            {"beats": 15, "octas": 3, "days": 1, "name": "test"},
+        ),
+        (
+            {"beats": 15, "octas": 2, "days": 1, "name": "test"},
+            {"beats": 15, "octas": 2, "days": 2, "name": "test"},
+        ),
+    ]
+
+    for test_a, test_b in not_equal:
+        tast_tc1 = RTSTimeComponent.from_units(**test_a)
+        test_tc2 = RTSTimeComponent.from_units(**test_b)
+        assert tast_tc1 != test_tc2
+
+    test_false = [
+        ({"beats": 15, "octas": 2, "name": "test"}, 0),
+        ({"beats": 15, "octas": 2, "days": 1, "name": "test"}, "a"),
+        ({"beats": 15, "octas": 2, "days": 1, "name": "test"}, 0.1),
+    ]
+    for test_a, value in test_false:
+        tast_tc1 = RTSTimeComponent.from_units(**test_a)
+        assert tast_tc1 != value
+
+
+def test_repr():
+    values = [
+        (
+            {"beats": 15, "octas": 2, "days": 1, "name": "test"},
+            "RSTimeComponent.from_units(days=1, octas=2, beats=15)",
+        ),
+        (
+            {"beats": 15, "octas": 2, "days": 1, "name": "test"},
+            "RSTimeComponent.from_units(days=1, octas=2, beats=15)",
+        ),
+        (
+            {"beats": 19, "octas": 4, "days": 13, "name": "test"},
+            "RSTimeComponent.from_units(days=13, octas=4, beats=19)",
+        ),
+    ]
+    for args, expected in values:
+        test_tc = RTSTimeComponent.from_units(**args)
+        assert repr(test_tc) == expected
+
+
+def test_from_timecomponent():
+    test_tc = RTSTimeComponent(134566, "base")
+
+    new_tc = RTSTimeComponent.from_time_component(test_tc)
+
+    assert new_tc == test_tc
+    assert new_tc.name == "base"
+
+    test_tc = RTSTimeComponent(134566, "base")
+
+    new_tc = RTSTimeComponent.from_time_component(test_tc, "test")
+
+    assert new_tc == test_tc
+    assert new_tc.name == "test"
+
+    new_tc = RTSTimeComponent.from_time_component(test_tc)
+
+    assert new_tc == test_tc
+    assert new_tc.name == "base"
+
+    new_tc = RTSTimeComponent.from_time_component(test_tc, "test")
+
+    assert new_tc == test_tc
+    assert new_tc.name == "test"
+
+
+def test_to_timestamp():
+    new_tc = RTSTimeComponent(134566, "base")
+    assert new_tc.to_timestamp() == "base:134566"
+
+    new_tc = RTSTimeComponent(1458777, "test")
+    assert new_tc.to_timestamp() == "test:1458777"
+
+    new_tc = RTSTimeComponent(1458777, "test_tc")
+    assert new_tc.to_timestamp() == "test_tc:1458777"
+
+
+def test_format():
+    format_str = "[b.RO]:[b.OC]:[b.HE]:[b.TA]:[b.DE]:[b.BE]/[b.RH]:[b.HO]:[b.MI]:[b.SE] [b.DA]-[b.YE]-[b.QU]"
+
+    new_tc = RTSTimeComponent(134566, "b")
+    assert new_tc.format(format_str) == "7:1:0:0:4:6/28:4:2:4 1-0-0"
+
+    new_tc = RTSTimeComponent(1458777, "b")
+    assert new_tc.format(format_str) == "75:3:7:4:9:17/303:15:54:42 12-0-0"
+
+    new_tc = RTSTimeComponent(1458777, "c")
+    assert (
+        new_tc.format(format_str)
+        == "[b.RO]:[b.OC]:[b.HE]:[b.TA]:[b.DE]:[b.BE]/[b.RH]:[b.HO]:[b.MI]:[b.SE] [b.DA]-[b.YE]-[b.QU]"
+    )
